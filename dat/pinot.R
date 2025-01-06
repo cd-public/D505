d467479db2465bdb331b976222b141c75fe17ea1
cd <cd>
2025-01-05 22:50:56 -0800

    index

--- dat/pinot.R
+++ dat/pinot.R
@@ -1,6 +1,5 @@
 library(tidyverse)
-setwd("~/GDrive/teaching/DATA-504/resources")
-wine <- read_rds("wine.rds") %>% 
+wine <- readRDS(gzcon(url("https://cd-public.github.io/courses/rmls25/dat/wine.rds"))) %>% 
   filter(variety %in% c("Pinot Noir","Burgundy","Pinot Nero")) %>% 
   mutate(province = fct_lump(province,5)) %>% 
   mutate(province = as.character(province)) %>% 
@@ -10,4 +9,4 @@ wine <- read_rds("wine.rds") %>%
   mutate(province = str_replace_all(province, " ", "_")) 
 
 as.data.frame(wine) %>% 
-  write_rds(path = "pinot.rds")
+  write_rds("pinot_orig.rds")
